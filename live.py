d28385905f2ec4b25aca1c287e44950aa7f16a31
MV-Python <46030721+MV-Python@users.noreply.github.com>
2019-03-09 21:16:45 -0500

    Update live.py

--- live.py
+++ live.py
@@ -1,18 +1,2 @@
-try:
-  from flask import Flask
-  from threading import Thread
-
-  app = Flask('')
-
-  @app.route('/')
-  def home():
-      return "Bot Online"
-
-  def run():
-    app.run(host='0.0.0.0',port=8080)
-
-  def keep_alive():  
-      t = Thread(target=run)
-      t.start()
-except:
-  pass
+def keep_alive():
+  print("Not Yet Implemented")
